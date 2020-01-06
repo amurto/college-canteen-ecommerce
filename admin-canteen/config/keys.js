eb6a1b1c4341a9845fab79eac789fd6e7e4c58b7
amurto <amurtobasu@gmail.com>
2020-01-06 14:15:53 +0530

    Update keys.js

--- admin-canteen/config/keys.js
+++ admin-canteen/config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://amurto:'+ encodeURIComponent('*mongodb*') + '@test0-vbxqa.mongodb.net/test?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://XXusernameXX:'+ encodeURIComponent('XXpasswordXX') + '@test0-vbxqa.mongodb.net/test?retryWrites=true&w=majority';
 module.exports = {
     mongoURI: dbPassword
 };
